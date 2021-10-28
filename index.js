26ecf597fad47be2ae3325966d1c49e5b4cd002d
Elizabeth Carolina <celerner4@gmail.com>
2021-10-28 19:56:26 -0300

    cambios nav

---
+++ index.js
@@ -0,0 +1,10 @@
+// ELEMENTOS DEL DOM
+
+const spanLigthMode = document.querySelector(".button-fa-sun")
+const spanDarkMode = document.querySelector(".button-fa-moon")
+const buttonDarkLigthMode = document.querySelector(".button-dark-mode")
+
+buttonDarkLigthMode.onclick = () =>{
+    spanLigthMode.classList.toggle("button-none")
+    spanDarkMode.classList.toggle("button-none")
+}
