f396b0f6e8070943629629e9bdda4b3d5b4eb2fc
Elizabeth Carolina <celerner4@gmail.com>
2021-10-28 22:58:54 -0300

    sección mis proyectos terminada

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // ELEMENTOS DEL DOM
 
-const spanLigthMode = document.querySelector(".button-fa-sun")
-const spanDarkMode = document.querySelector(".button-fa-moon")
-const buttonDarkLigthMode = document.querySelector(".button-dark-mode")
+const spanLigthMode = document.querySelector(".button-fa-sun");
+const spanDarkMode = document.querySelector(".button-fa-moon");
+const buttonDarkLigthMode = document.querySelector(".button-dark-mode");
 
 buttonDarkLigthMode.onclick = () =>{
     spanLigthMode.classList.toggle("button-none")
