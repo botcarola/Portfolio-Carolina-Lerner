39d6f61e81b6a20acfe0ec6edc026784c38ddfe4
Elizabeth Carolina <celerner4@gmail.com>
2021-11-02 13:32:38 -0300

    agrego :focus a elementos html

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // ELEMENTOS DEL DOM
 
-const spanLigthMode = document.querySelector(".button-fa-sun");
+const spanLightMode = document.querySelector(".button-fa-sun");
 const spanDarkMode = document.querySelector(".button-fa-moon");
 const buttonDarkLigthMode = document.querySelector(".button-dark-mode");
 const buttonContactMe = document.querySelector("#b-contact")
 
 
 buttonDarkLigthMode.onclick = () => {
-    spanLigthMode.classList.toggle("button-none")
-    spanDarkMode.classList.toggle("button-none")
+spanLightMode.classList.toggle("button-none")
+spanDarkMode.classList.toggle("button-none")
 }
 
 buttonContactMe.onclick = () => {
-    window.location.hash = "contacto"
-
+window.location.hash = "contacto"
 }
