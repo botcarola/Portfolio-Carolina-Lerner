ff7fd07c9c4937558bf659dd1664b30f6c791fac
Elizabeth Carolina <celerner4@gmail.com>
2021-11-09 17:26:47 -0300

    comienzo modo oscuro

--- index.js
+++ index.js
@@ -4,13 +4,35 @@ const spanLightMode = document.querySelector(".button-fa-sun");
 const spanDarkMode = document.querySelector(".button-fa-moon");
 const buttonDarkLigthMode = document.querySelector(".button-dark-mode");
 const buttonContactMe = document.querySelector("#b-contact")
+const buttonProjects = document.querySelector("#buttonProjects")
+const body = document.querySelector("body")
 
+
+// MODO OSCURO
+
+console.log("hola")
 
 buttonDarkLigthMode.onclick = () => {
 spanLightMode.classList.toggle("button-none")
 spanDarkMode.classList.toggle("button-none")
+
+if(spanDarkMode.classList.contains("button-none")){
+    body.classList.remove("light-mode")
+    body.classList.add("dark-mode")
+} else {
+    body.classList.remove("dark-mode")
+    body.classList.add("light-mode")    
+ }
 }
 
-buttonContactMe.onclick = () => {
-window.location.hash = "contacto"
+
+// BOTONES && ENLACES DE ANCLA
+
+const buttonWindowLocation = (boton, ancla) => {
+boton.onclick = () => {
+window.location.hash = ancla
 }
+}
+
+buttonWindowLocation(buttonContactMe, "contacto")
+buttonWindowLocation(buttonProjects, "proyectos")
